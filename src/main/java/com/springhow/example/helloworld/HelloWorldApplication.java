fdd515fdc84632a607693c6c7c638ec2e86b9ec6
Sohumnm <158448220+Sohumnm@users.noreply.github.com>
2024-08-09 19:27:54 +0530

    Update HelloWorldApplication.java

--- src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
@@ -24,8 +24,6 @@ public class HelloWorldApplication extends SpringBootServletInitializer {
     @RequestMapping("/")
     String helloWorld() {
         return "TESTING SUCCESSFULL";
-                    "TESTING SUCCESSFULL";
-                        "TESTING SUCCESSFULL";
     }
 
 }
