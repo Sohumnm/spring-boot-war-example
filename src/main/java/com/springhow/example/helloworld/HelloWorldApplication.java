b24818def1f176a28ef287ae4dd6b921a780cb3f
Sohumnm <158448220+Sohumnm@users.noreply.github.com>
2024-08-27 20:15:29 +0530

    Update HelloWorldApplication.java

--- src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
@@ -23,7 +23,7 @@ public class HelloWorldApplication extends SpringBootServletInitializer {
 
     @RequestMapping("/")
     String helloWorld() {
-        return "Congratulation, successfully initiated CICD using GIT,JENKINS,ANSIBLE,";
+        return "Congratulation, successfully initiated CICD using GIT,JENKINS,ANSIBLE end to end. Project2 complete!";
     }
 
 }
